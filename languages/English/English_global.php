b8d591b93dd0691bb9d9e9fca7bd60f10d4dd0c6
rica-carv <rica-carv@users.noreply.github.com>
2025-09-21 18:18:54 +0100

    Delete languages directory

--- languages/English/English_global.php
+++
@@ -1,4 +0,0 @@
-<?php
-// O LAN_PLUGIN_PHILLIS_PAGE_NAME tem de ficar, por causa dos títulos da página nos browsers
-define("LAN_PLUGIN_PHILLIS_PAGE_NAME","Phil (Lists)");
-define("LAN_PLUGIN_PHILLIST_DESCRIPTION","This plugin is a fully featured Philatelic List system.");
--- languages/Portuguese/Portuguese_global.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("LAN_PLUGIN_PHILLIS_PAGE_NAME","Filatelia - Listas");
-define("LAN_PLUGIN_PHILLIS_DESCRIPTION","Este plugin é um sistema de Listas Filatélico com todos os recursos.");
